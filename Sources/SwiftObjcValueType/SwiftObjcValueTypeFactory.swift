f8b6cf8a75a2b9a885f2d000170c9ba3d795932e
Sihao Lu <slu2@snapchat.com>
2024-07-22 15:38:15 -0700

    Tweak the objc builder generation - matchers can be nil

--- Sources/SwiftObjcValueType/SwiftObjcValueTypeFactory.swift
+++ Sources/SwiftObjcValueType/SwiftObjcValueTypeFactory.swift
@@ -583,9 +583,9 @@ public struct SwiftObjcValueTypeFactory {
                         FunctionParameterSyntax(
                             firstName: index == 0 ? .wildcardToken() : caseElement.name,
                             secondName: index == 0 ? caseElement.name : nil,
-                            type:IdentifierTypeSyntax(
+                            type: IdentifierTypeSyntax(
                                 name: .identifier("\(enumName)\(caseElement.name.trimmed.text.uppercasingFirst)MatchHandler")
-                            )
+                            ).optionalized
                         )
                     }
                 }
@@ -608,8 +608,10 @@ public struct SwiftObjcValueTypeFactory {
                         )
                     ) {
                         FunctionCallExprSyntax(
-                            calledExpression: DeclReferenceExprSyntax(
-                                baseName: caseElement.name
+                            calledExpression: OptionalChainingExprSyntax(
+                                expression: DeclReferenceExprSyntax(
+                                    baseName: caseElement.name.trimmed
+                                )
                             ),
                             leftParen: .leftParenToken(),
                             rightParen: .rightParenToken()
