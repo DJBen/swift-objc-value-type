1e8c6500b4e26a0588aba04543bb442cdb1abe8f
Sihao Lu <slu2@snapchat.com>
2024-08-02 03:19:58 -0700

    Revert "Fix equality checking for optional NSObjects"
    This reverts commit eee05af8211c4a196c8865989184bbd8e2bcfbca.

--- Sources/SwiftObjcValueType/SwiftObjcValueTypeFactory+isEqual.swift
+++ Sources/SwiftObjcValueType/SwiftObjcValueTypeFactory+isEqual.swift
@@ -54,21 +54,91 @@ extension SwiftObjcValueTypeFactory {
         type: some TypeSyntaxProtocol,
         valueObjectConfig: ValueObjectConfig
     ) -> ExprListSyntax {
-        DeclReferenceExprSyntax(
-            baseName: identifier
-        )
-
-        BinaryOperatorExprSyntax(operator: .binaryOperator("=="))
-
-        MemberAccessExprSyntax(
-            base: DeclReferenceExprSyntax(
-                baseName: .identifier("other")
-            ),
-            period: .periodToken(),
-            declName: DeclReferenceExprSyntax(
+        // Use `a == other.a` if primitive type
+        // Otherwise use `a.isEqual(other.a)` if not optional
+        // `a?.isEqual(other?.a) == true` if optional
+        if type.asEnumTypeIfKnown(
+            nsEnumTypes: valueObjectConfig.nsEnumTypes
+        ).0.shouldUseEqualSignForEqualityCheck {
+            DeclReferenceExprSyntax(
                 baseName: identifier
             )
-        )
+
+            BinaryOperatorExprSyntax(operator: .binaryOperator("=="))
+
+            MemberAccessExprSyntax(
+                base: DeclReferenceExprSyntax(
+                    baseName: .identifier("other")
+                ),
+                period: .periodToken(),
+                declName: DeclReferenceExprSyntax(
+                    baseName: identifier
+                )
+            )
+        } else if type.is(OptionalTypeSyntax.self) {
+            FunctionCallExprSyntax(
+                calledExpression: MemberAccessExprSyntax(
+                    base: OptionalChainingExprSyntax(
+                        expression: DeclReferenceExprSyntax(
+                            baseName: identifier
+                        ),
+                        questionMark: .postfixQuestionMarkToken()
+                    ),
+                    period: .periodToken(),
+                    declName: DeclReferenceExprSyntax(
+                        baseName: .identifier("isEqual")
+                    )
+                ),
+                leftParen: .leftParenToken(),
+                arguments: LabeledExprListSyntax {
+                    LabeledExprSyntax(
+                        expression: MemberAccessExprSyntax(
+                            base: DeclReferenceExprSyntax(
+                                baseName: .identifier("other")
+                            ),
+                            period: .periodToken(),
+                            declName: DeclReferenceExprSyntax(
+                                baseName: identifier
+                            )
+                        )
+                    )
+                },
+                rightParen: .rightParenToken()
+            )
+
+            BinaryOperatorExprSyntax(operator: .binaryOperator("=="))
+
+            BooleanLiteralExprSyntax(literal: .keyword(.true))
+
+        } else {
+            FunctionCallExprSyntax(
+                calledExpression: MemberAccessExprSyntax(
+                    base: DeclReferenceExprSyntax(
+                        baseName: identifier
+                    ),
+                    period: .periodToken(),
+                    declName: DeclReferenceExprSyntax(
+                        baseName: .identifier("isEqual")
+                    )
+                ),
+                leftParen: .leftParenToken(),
+                rightParen: .rightParenToken()
+            ) {
+                LabeledExprListSyntax {
+                    LabeledExprSyntax(
+                        expression: MemberAccessExprSyntax(
+                            base: DeclReferenceExprSyntax(
+                                baseName: .identifier("other")
+                            ),
+                            period: .periodToken(),
+                            declName: DeclReferenceExprSyntax(
+                                baseName: identifier
+                            )
+                        )
+                    )
+                }
+            }
+        }
     }
 
     @MemberBlockItemListBuilder
@@ -170,5 +240,39 @@ extension SwiftObjcValueTypeFactory {
         }
         
         return exprList
+    }
+}
+
+private extension TypeSyntaxProtocol {
+    var shouldUseEqualSignForEqualityCheck: Bool {
+        if isObjcPrimitive {
+            return true
+        }
+
+        if self.is(ArrayTypeSyntax.self) {
+            return true
+        }
+
+        if self.is(DictionaryTypeSyntax.self) {
+            return true
+        }
+        
+        if let idType = self.as(IdentifierTypeSyntax.self), ObjcSwiftUtils.swiftToObjcFoundationTypeMap[idType.name.trimmed.text] != nil {
+            return true
+        }
+
+        if self.isIdentifierTypeEqual("UUID") {
+            return true
+        }
+
+        if self.isIdentifierTypeEqual("URL") {
+            return true
+        }
+
+        if let optional = self.as(OptionalTypeSyntax.self) {
+            return optional.wrappedType.shouldUseEqualSignForEqualityCheck
+        }
+
+        return false
     }
 }
